7106d396f7f5e4d847ea343200e39f5a1246c7a8
storialp <96848889+storialp@users.noreply.github.com>
2023-09-25 19:00:01 +0200

    yeah

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <h1 className="text-md">Days since last JS framework</h1>
-      <h1 className="text-2xl">0</h1>
+      <h1 className="text-md mx-auto">Days since last JS framework</h1>
+      <h1 className="text-2xl mx-auto">0</h1>
       <a
         href="https://github.com/storialp/days-since-js-framework-copy"
         className="underline"
