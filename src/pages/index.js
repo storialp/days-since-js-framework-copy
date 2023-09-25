157277646a641a946a999ca441bfa470be1f24d0
storialp <96848889+storialp@users.noreply.github.com>
2023-09-25 18:58:01 +0200

    yeah

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <main>
       <h1 className="text-md">Days since last JS framework</h1>
       <h1 className="text-2xl">0</h1>
     </main>
